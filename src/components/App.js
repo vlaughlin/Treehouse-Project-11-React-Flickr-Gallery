3ee813663468efc7d08d847a47c4f69f6bf65304
vlaughlin <vrodriguez10@gmail.com>
2018-01-07 16:28:39 -0500

    setting up components folder
    made changes to app,js and index.js

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom'
+
+
+
+const App = () => (
+<BrowserRouter>
+<div className="container">
+
+
+</div>
+</BrowserRouter>
+
+);
+
+
+export default App;
