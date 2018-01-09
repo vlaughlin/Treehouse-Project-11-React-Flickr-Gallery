d036a4873a7bb33aa2a66393c6c878e64c03ff96
vlaughlin <vrodriguez10@gmail.com>
2018-01-08 22:38:57 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
 import './css/index.css';
-
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
 
 document.getElementById('root'));
+registerServiceWorker();
