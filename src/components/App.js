bf362eb0da77373269a2a4d2d98f4cc9eda25ea5
vlaughlin <vrodriguez10@gmail.com>
2018-01-08 20:51:12 -0500

    setting routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {
   BrowserRouter,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
 
+//App components
+import Cats from './Cats';
+import Dogs from './Dogs';
+import Computers from './Computers';
 
-
-const App = () => (
+ const App = () => (
 <BrowserRouter>
 <div className="container">
-
-
+<Route exact path= "/cats" component={Cats} />
+<Route exact path= "/dogs" component={Dogs} />
+<Route exact path= "/computers" component={Computers} />
 </div>
 </BrowserRouter>
-
 );
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
   <App />,
 
 document.getElementById('root'));
-registerServiceWorker();
