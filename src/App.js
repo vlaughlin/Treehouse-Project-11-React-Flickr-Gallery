ac2f03bf1a23a5c9509714b10d743e08a09f936f
vlaughlin <vrodriguez10@gmail.com>
2018-01-09 20:25:23 -0500

    added <Link> Component for navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 
 //App components
+import SearchForm from './components/navigation/SearchForm';
 import Cats from './components/navigation/Cats';
 import Dogs from './components/navigation/Dogs';
 import Computers from './components/navigation/Computers';
@@ -12,6 +13,7 @@ import Computers from './components/navigation/Computers';
  const App = () => (
 <BrowserRouter>
 <div className="container">
+<Route exact path= "/" component={SearchForm} />
 <Route exact path= "/cats" component={Cats} />
 <Route exact path= "/dogs" component={Dogs} />
 <Route exact path= "/computers" component={Computers} />
