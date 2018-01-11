bacb466c170dd2e191c4d802d2f0b5c8ddf030fe
vlaughlin <vrodriguez10@gmail.com>
2018-01-10 19:54:35 -0500

    created component format
    removed SearchForm.js and NotFound.js components and route paths  from
    App.js (will work on those later)

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import {
 } from 'react-router-dom';
 
 //App components
-import SearchForm from './components/navigation/SearchForm';
 import Cats from './components/navigation/Cats';
 import Dogs from './components/navigation/Dogs';
 import Computers from './components/navigation/Computers';
-import NotFound from './components/navigation/NotFound';
+
 
  const App = () => (
 <BrowserRouter>
 <div className="container">
 <Switch>
-<Route exact path= "/" component={SearchForm} />
 <Route exact path= "/cats" component={Cats} />
 <Route exact path= "/dogs" component={Dogs} />
 <Route exact path= "/computers" component={Computers} />
-<Route component={NotFound} />
+
 </Switch>
 </div>
 </BrowserRouter>
