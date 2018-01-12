49473f1f2af1b61958d42d1f82969df63ac26e81
Valerie Laughlin <vrodriguez10@gmail.com>
2018-01-11 21:07:01 -0500

    Update Computers.js

--- src/components/navigation/Computers.js
+++ src/components/navigation/Computers.js
@@ -1,12 +1,10 @@
-// Component for Computers category
+
 import React from 'react';
 
-// Import Components used
+
 import Nav from './Nav';
 
 
-// Passes the "computers' query into the Container Component via props
-// Returns the Nav and Container Components to the DOM
 const Computers = props => {
   return (
     <div>
