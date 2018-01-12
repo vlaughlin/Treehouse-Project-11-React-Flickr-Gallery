8c8e6a5f878958aae789e6d51e6c3940f04164c1
vlaughlin <vrodriguez10@gmail.com>
2018-01-11 21:09:46 -0500

    removed Nav from category

--- src/components/navigation/Cats.js
+++ src/components/navigation/Cats.js
@@ -1,12 +1,10 @@
-// Component for Cats category
+
 import React from 'react';
 
-// Import Components used
-import Nav from './Nav';
 
 
-// Passes the "cats' query into the Container Component via props
-// Returns the Nav and Container Components to the DOM
+
+
 const Cats = props => {
   return (
     <div>
--- src/components/navigation/Dogs.js
+++ src/components/navigation/Dogs.js
@@ -1,17 +1,13 @@
 
-// Component for Dogs category
+
 import React from 'react';
 
-// Import Components used
-import Nav from './Nav';
 
 
-// Passes the "dogs' query into the Container Component via props
-// Returns the Nav and Container Components to the DOM
 const Dogs = props => {
   return (
     <div>
-  
+
 
     </div>
   )
