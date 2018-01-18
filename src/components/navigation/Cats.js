42bf0511a2c8be027dd870f9e97367bd396ab62d
vlaughlin <vrodriguez10@gmail.com>
2018-01-17 22:16:56 -0500

    cats.js

--- src/components/navigation/Cats.js
+++ src/components/navigation/Cats.js
@@ -1,11 +1,12 @@
-
 import React from 'react';
+import Container from '../Container';
 
 
 const Cats = props => {
   return (
     <div>
-
+    <h2>{props.query}</h2>
+    <Container query={"cats"} />
 
     </div>
   )
