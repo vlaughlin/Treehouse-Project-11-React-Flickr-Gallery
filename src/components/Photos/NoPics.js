50268045e60b4cb234439493fc59978a3e945b13
vlaughlin <vrodriguez10@gmail.com>
2018-01-17 23:42:12 -0500

    done

--- src/components/Photos/NoPics.js
+++ src/components/Photos/NoPics.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NoPics = props => (
 
     <li className='no-pics'>
-    <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
-    <h3> Sorry, no GIFs match your search.</h3>
+
+    <h3> :( Sorry, no GIFs match your search.</h3>
     </li>
 );
 
