1cb7d88334889d072998561325c64a5b30b2e839
vlaughlin <vrodriguez10@gmail.com>
2018-01-17 21:11:34 -0500

    made PicList import relative to container.js

--- src/components/container.js
+++ src/components/container.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
-import PicList from './components/Photos/PicList';
+import PicList from './Photos/PicList';
 
 import apiKey from '../config.js';
 
