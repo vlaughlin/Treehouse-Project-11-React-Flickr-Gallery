0fe7673066f4a56af948a3749807b1c987980744
vlaughlin <vrodriguez10@gmail.com>
2018-01-24 21:54:09 -0500

    comment

--- src/components/navigation/Cats.js
+++ src/components/navigation/Cats.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Container from '../Container';
 
 //The cats query is passed into the Container component via props
-const Cats = props =>
+
+const Cats = props => {
   return (
     <div>
     <h2>{props.query}</h2>
